8ef4f3d546f7091a2a3aba914334deb7843e1c98
xpectro93 <jonathanandrade@pursuit.org>
2020-03-12 14:37:25 -0400

    add appjs file on backend

---
+++ backend/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3001;
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({extended :false}));
+app.use(bodyParser.json());
+
+app.use('/', (req, res, next) => {
+  res.status(200).json({
+    
+  })
+})
+
+//error handling;
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    err
+  });
+});
+
+app.listen(port, () => console.log('Listening on =>', port));
+
